d2f1b3fc1d2379001b49077df4fd107976b4eef6
Disha <savaliyanikhil36@gmail.com>
2026-03-01 20:16:52 +0530

    add file

---
+++ employee/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render,redirect
+from .models import Employee
+
+
+# Create your views here.
+def emp_list(request):
+    emp=Employee.objects.all()
+    return render(request,"emp_details.html",{"emp":emp})
+
+def emp_add(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        salary=request.POST.get("salary")
+        department=request.POST.get("department")
+        Employee.objects.create(name=name,email=email,salary=salary,department=department)
+        return redirect("emp_list")
+    return render(request,"add.html")
+
+def emp_update(request,id):
+    emp=Employee.objects.get(id=id)
+    if request.method == "POST":
+        
+        emp.name=request.POST.get('name')
+        emp.email=request.POST.get('email')
+        emp.salary=request.POST.get('salary') 
+        emp.department=request.POST.get("department")
+        emp.save()
+        return redirect('emp_list')
+    return render(request,'update.html',{'emp':emp})
+
+def emp_delete(request,id):
+    emp=Employee.objects.get(id=id)
+    emp.delete()
+    return redirect('emp_list')
+
+
+
